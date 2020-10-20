760cfa0148c3a5296e0bf4aab71b16640a0e1aa0
everson-souza <everson@alunos.utfpr.edu.br>
2020-10-20 12:11:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="light-green"
+      
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          :src="require('../src/assets/lakes_logo.png')"
+          transition="scale-transition"
+          width="70"
+        />
+
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <project/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld';
+import Project from './components/Project';
+
+export default {
+  name: 'App',
+
+  components: {
+  //  HelloWorld,
+    Project
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Project.vue
@@ -0,0 +1,191 @@
+<template>
+   <v-container>
+    <v-row justify="space-around">
+      <v-col cols="3">   
+        <v-card
+        :loading="loading"
+        class="mx-auto my-12"
+        max-width="300"
+        elevation="5"
+        >
+        <template slot="progress">
+            <v-progress-linear
+            color="green"
+            height="5"
+            indeterminate
+            ></v-progress-linear>
+        </template>
+    
+        <v-card-title>BOEM</v-card-title>
+    
+        <v-card-text>
+            <div>Outer Continental Shelf (OCS) Air Quality System</div>
+        </v-card-text>
+        
+        <v-card-text>
+            <div class="font-weight-bold ml-8 mb-2">
+                Browsers
+            </div>
+            <v-timeline                
+                dense
+            >
+                <v-timeline-item
+                v-for="message in messages"
+                :key="message.time"
+                :color="message.color"    
+                class="browser"            
+                small
+                >
+                <template v-slot:icon>
+                <i :class="message.icon"></i>
+                </template>
+                <div>
+                    <div class="font-weight-normal">
+                    <strong>{{ message.from }}</strong> 
+                    </div>
+                    <div>Latest run was @ {{ message.time }}</div>
+                </div>
+                </v-timeline-item>
+            </v-timeline>
+        </v-card-text> 
+        <v-card-actions>
+            <v-btn
+            v-for="message in messages"
+                :key="message.time"
+                :color="message.color"                                
+                text
+                @click="reserve"
+            >
+                {{message.from}}
+            </v-btn>
+            <v-btn                
+                text
+                color="green"
+                @click="reserve"
+            >
+                508
+            </v-btn>
+        </v-card-actions>       
+        </v-card>
+    </v-col>
+    <v-col cols="3">   
+        <v-card
+        :loading="loading"
+        class="mx-auto my-12"
+        max-width="300"
+        elevation="5"
+        >
+        <template slot="progress">
+            <v-progress-linear
+            color="green"
+            height="5"
+            indeterminate
+            ></v-progress-linear>
+        </template>
+    
+        <v-card-title>COSD</v-card-title>
+        <v-card-text>
+            <div>San Diego APCD - Emissions Inventory System (EIS)</div>
+        </v-card-text>
+        <v-card-text>
+            <div class="font-weight-bold ml-8 mb-2">
+                Browsers                
+            </div>
+            
+            <v-timeline                
+                dense
+            >
+                <v-timeline-item
+                v-for="message in messages"
+                :key="message.time"
+                :color="message.color"                
+                class="browser"
+                small
+                >
+                <template v-slot:icon>
+                <i :class="message.icon"></i>
+                </template>
+                <div>                  
+                    <div class="font-weight-normal">
+                    <strong>{{ message.from }}</strong> 
+                    </div>
+                    <div>Latest run was @ {{ message.time }}</div>
+                </div>
+                </v-timeline-item>
+            </v-timeline>
+        </v-card-text>    
+        <v-card-actions>
+            <v-btn
+            v-for="message in messages"
+                :key="message.time"
+                :color="message.color"                                
+                text
+                @click="reserve"
+            >
+                {{message.from}}
+            </v-btn>
+            <v-btn                
+                text
+                color="green"
+                @click="reserve"
+            >
+                508
+            </v-btn>
+        </v-card-actions>
+        </v-card>
+    </v-col>
+    <v-col cols="3">
+    </v-col>
+    <v-col cols="3">
+    </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'Project',
+        data: () => ({
+            hover: false,
+            loading: false,
+            selection: 1,
+            
+            
+            messages: [
+                {
+                from: 'Chrome',            
+                icon:'fab fa-chrome',
+                time: '10:42am 19/10/2020',
+                color: 'light-blue lighten-1',
+                },
+                {
+                from: 'Firefox',
+                icon:'fab fa-firefox-browser',
+                time: '10:37am 19/10/2020',
+                color: 'orange',
+                }                
+            ],
+        }),
+        methods: {
+            reserve () {                
+                const { exec } = require('child_process');
+                exec('cat *.js bad_file | wc -l', (err, stdout, stderr) => {
+                if (err) {
+                    console.log("erro");
+                    // node couldn't execute the command
+                    return;
+                }
+
+                // the *entire* stdout and stderr (buffered)
+                console.log('stdout: ${stdout}');
+                console.log('stderr: ${stderr}');
+                });
+              
+                this.loading = true                
+                setTimeout(() => (this.loading = false), 2000)
+            },
+        },
+    }
+</script>  
+<style>    
+</style>
