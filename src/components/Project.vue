81ccd65c61484331d7865cfb87c3c2edf8bc9db4
everson-souza <everson@alunos.utfpr.edu.br>
2020-10-22 11:44:55 -0300

    Got back

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,105 +1,86 @@
 <template>
    <v-container>
-        <v-row justify="space-around">
-            <v-col 
-                cols="12"        
-                xs="12"
-                sm="6"
-                md="4"
-                lg="4"
-                xl="6"
-                v-for="project in projects"
-                :key="project.name"  
-            >   
-                <v-card
-                :loading="loading"
-                class="mx-auto my-12"
-                max-width="400"
-                elevation="5"
+    <v-row justify="space-around">
+      <v-col 
+        cols="12"        
+        xs="12"
+        sm="6"
+        md="4"
+        lg="4"
+        xl="6"
+        v-for="project in projects"
+        :key="project.name"  
+      >   
+        <v-card
+        :loading="loading"
+        class="mx-auto my-12"
+        max-width="400"
+        elevation="5"
+        >
+        <template slot="progress">
+            <v-progress-linear
+            color="green"
+            height="5"
+            indeterminate
+            ></v-progress-linear>
+        </template>
+    
+        <v-card-title>{{project.name}}</v-card-title>
+    
+        <v-card-text>
+            <div>{{project.text}}</div>
+        </v-card-text>
+        
+        <v-card-text>
+            <div class="font-weight-bold ml-8 mb-2">
+                Browsers
+            </div>
+            <v-timeline                
+                dense
+            >
+                <v-timeline-item
+                v-for="browser in project.browsers"
+                :key="browser.time"
+                :color="browser.color"    
+                class="browser"            
+                small
                 >
-                <template slot="progress">
-                    <v-progress-linear
-                    color="green"
-                    height="5"
-                    indeterminate
-                    ></v-progress-linear>
+                <template v-slot:icon>
+                <i :class="browser.icon"></i>
                 </template>
-            
-                <v-card-title>{{project.name}}</v-card-title>
-            
-                <v-card-text>
-                    <div>{{project.text}}</div>
-                </v-card-text>
-                
-                <v-card-text>
-                    <div class="font-weight-bold ml-8 mb-2">
-                        Browsers
+                <div>
+                    <div class="font-weight-normal">
+                    <strong>{{ browser.from }}</strong> 
                     </div>
-                    <v-timeline                
-                        dense
-                    >
-                        <v-timeline-item
-                        v-for="browser in project.browsers"
-                        :key="browser.time"
-                        :color="browser.color"    
-                        class="browser"            
-                        small
-                        >
-                        <template v-slot:icon>
-                        <i :class="browser.icon"></i>
-                        </template>
-                        <div>
-                            <div class="font-weight-normal">
-                            <strong>{{ browser.from }}</strong> 
-                            </div>
-                            <div>Latest run was @ {{ browser.time }}</div>
-                        </div>
-                        </v-timeline-item>
-                    </v-timeline>
-                </v-card-text> 
-                <v-card-actions>            
-                    <v-btn
-                        v-for="browser in project.browsers"
-                        :key="browser.time"   
-                        :color="browser.color"                                
-                        text
-                        :disabled="browser.disabled"
-                        @click="browser.type == '1' ? (reserve(project.name.toLowerCase(), browser.from.toLowerCase())) : (download(project.name.toLowerCase()))">
-                        {{browser.from}}             
-                    </v-btn>                       
-                </v-card-actions>
-                </v-card>
-                <v-snackbar
-                    v-model="snackbar"
-                    :timeout="2000"
-                    >
-                    {{ text }}
-
-                    <template v-slot:action="{ attrs }">
-                        <v-btn
-                        color="indigo"
-                        text
-                        v-bind="attrs"
-                        @click="snackbar = false"
-                        >
-                        Close
-                        </v-btn>
-                    </template>
-                    </v-snackbar>
-            </v-col>    
-        </v-row>        
+                    <div>Latest run was @ {{ browser.time }}</div>
+                </div>
+                </v-timeline-item>
+            </v-timeline>
+        </v-card-text> 
+        <v-card-actions>            
+            <v-btn
+                v-for="browser in project.browsers"
+                :key="browser.time"   
+                :color="browser.color"                                
+                text
+                :disabled="browser.disabled"
+                @click="browser.type == '1' ? (reserve(project.name.toLowerCase(), browser.from.toLowerCase())) : (download(project.name.toLowerCase()))">
+                {{browser.from}}             
+            </v-btn>           
+            
+        </v-card-actions>
+        </v-card>
+    </v-col>    
+    </v-row>    
+    
   </v-container>
 </template>
 
 <script>
     
-    import axios from "axios";
-    
     export default {
         name: 'Project',                
         data: () => ({
-            snackbar: false,
-            text: 'Sorry, this is not yet available',
             hover: false,
             loading: false,
             selection: 1,
@@ -141,54 +122,48 @@
                         icon:'fab fa-chrome',
                         time: '10:42am 19/10/2020',
                         color: 'light-blue lighten-1',
-                        type:'1',
+                        type: '1',
                         },
                         {
                         from: 'Firefox',
                         icon:'fab fa-firefox-browser',
                         time: '10:37am 19/10/2020',
                         color: 'orange',
-                        type:'1',
+                        type: '1',
+                        disabled: true,
                         },
                         {
                         from: '508',
                         icon:'',
                         time: '10:37am 19/10/2020',
                         color: 'green',
-                        type: '2',                        
+                        type: '2',
+                        disabled: true
                         }                
                     ],
                 }  
             ],
         }),
         methods: {
-            async reserve (project, browser) {        
-                axios
-                    .get('/projects/'+project+'/'+browser+'/allure-report/index.html')
-                    .then(response => {
-                        window.open(
-                            'projects/'+project+'/'+browser+'/allure-report/index.html',
-                            '_blank' // <- This is what makes it open in a new window.
-                        )
-                    })
-                    .catch(error => {
-                        this.snackbar=true;
-                        console.log('./../../public/projects/'+project+'/'+browser+'/allure-report/index.html');
-                    })                    
+            reserve (project, browser) {
+                
+                window.open(
+                    'projects/'+project+'/'+browser+'/allure-report/index.html',
+                    '_blank' // <- This is what makes it open in a new window.
+                );  
+        
             },
-            async download(project){                                                
-                axios
-                    .get('/projects/'+project+'/508/508.zip')
-                    .then(response => {         
-                        window.open(
-                            './../projects/'+project+'/508/508.zip',
-                            '_blank' // <- This is what makes it open in a new window.
-                        )
-                    })
-                    .catch(error => {
-                        this.snackbar=true;
-                        console.log('./../../public/projects/'+project+'/508/508.zip');
-                    })                
+            download(project){                                
+                                
+                try{                    
+                    window.open(
+                        './../projects/'+project+'/508/508.zip',
+                        '_blank' // <- This is what makes it open in a new window.
+                    );
+                }catch(err){
+                    console.log("erro");
+                }
+                
             }
         },          
     }
