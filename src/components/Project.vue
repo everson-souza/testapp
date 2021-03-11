f5f27a97efe8ecb266d419c7e16ee517425dacbc
everson-souza <everson@alunos.utfpr.edu.br>
2021-03-10 23:41:05 -0300

    Update on firefox

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -171,7 +171,7 @@
                         {
                         from: 'Firefox',
                         icon:'fab fa-firefox-browser',
-                        time: '07:56pm 03/08/2021',
+                        time: '11:02pm 03/10/2021',
                         color: 'orange',
                         },
                         {
