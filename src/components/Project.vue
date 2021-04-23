fda636f235ab548bef527545adc344b8de936f75
sysop-weblakes <79984869+sysop-weblakes@users.noreply.github.com>
2021-04-23 04:38:13 -0700

    Update on boem firefox

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -171,7 +171,7 @@
                         {
                         from: 'Firefox',
                         icon:'fab fa-firefox-browser',
-                        time: '04/21/2021',
+                        time: '04/23/2021',
                         color: 'orange',
                         },
                         {
