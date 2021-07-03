3a60698be4638843762c3c017f600dccb0e399d2
Everson de Souza <everson.3397@gmail.com>
2021-07-03 18:01:39 -0300

    Testing again

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: '138.204.26.122:3080',
+        target: 'http://138.204.26.122:3080',
         changeOrigin: true
       },
     }
