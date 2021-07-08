df7801560fc824818c0132961b61da7b092a2f9b
Everson de Souza <everson.3397@gmail.com>
2021-07-08 11:51:16 -0300

    Updated rtt - chrome

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -366,7 +366,7 @@
                     getProjects().then(response => {
                         //console.log(response)
                         this.projects = response? response : this.projects       
-                        console.log(response)
+                        console.log("Response:"+response)
                         console.log(this.projects)
                         
                     }) 
