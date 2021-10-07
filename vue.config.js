e55bb84f305f53e6da450546ddfee891aca54783
sysop-weblakes <79984869+sysop-weblakes@users.noreply.github.com>
2021-10-07 11:54:39 -0700

    Update vue.config file

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:3080',
+        target: 'http://localhost',
         changeOrigin: true
       }      
     },
